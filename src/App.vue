86aef47679c8b89c88cc9b20dd6b3c2b7b3f901d
Ahn-Gihwan <ahngh0113@naver.com>
2021-11-19 16:46:47 +0900

    config: vue-webpack-template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     Hello Vue
+  </div>
+</template>
+
+<script>
+console.log('App.vue')
+export default {
+
+}
+</script>
+
+<style>
+  div {
+    background-color: white;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App";
+
+import "./css/css.css";
+
+createApp(App).mount("#app");
